3ca9b1d574c49a1f4904733d609d837809bb9fae
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2025-06-13 16:25:49 +0200

    first commit initial

---
+++ resources/js/frontend/layouts/MainLayout.jsx
@@ -0,0 +1,7 @@
+export default function MainLayout({ children }) {
+    return (
+        <>
+            <div className="mt-14">{children}</div>
+        </>
+    );
+}
---
+++ resources/js/frontend/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="container bg-red-500 mx-auto">
+            <h1 className="text-center mt-5">Welcome to the Home Page</h1>
+            <p className="text-center">
+                This is a simple home page built with React and Inertia.js.
+            </p>
+        </div>
+    );
+}
