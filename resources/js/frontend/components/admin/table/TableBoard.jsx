b4bd5792f08156727f1da2f8522d6e66310a4cf9
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2025-07-12 21:49:00 +0200

    TableBoard width upgrade

--- resources/js/frontend/components/admin/table/TableBoard.jsx
+++ resources/js/frontend/components/admin/table/TableBoard.jsx
@@ -2,7 +2,7 @@ import DefaultTable from "./DefaultTable";
 
 export default function TableBoard({ tables }) {
     return (
-        <div className="relative w-full h-[500px] border border-gray-300  bg-[repeating-linear-gradient(90deg,_rgba(255,255,255,0.9)_0px,_rgba(255,255,255,0.9)_10px,_rgba(204,251,241,0.15)_10px,_rgba(204,251,241,0.15)_20px)] rounded">
+        <div className="relative w-[1200px] h-[500px] overflow-scroll border border-gray-300  bg-[repeating-linear-gradient(90deg,_rgba(255,255,255,0.9)_0px,_rgba(255,255,255,0.9)_10px,_rgba(204,251,241,0.15)_10px,_rgba(204,251,241,0.15)_20px)] rounded">
             {tables.map((table) => (
               <DefaultTable key={table.id} table={table}  />
             ))}
